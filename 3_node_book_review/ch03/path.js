79d31bb28d3cbd6fb14f0911964fe6c52ad94030
uriyang <girlstoryk@gmail.com>
2018-12-02 01:32:34 +0900

    열실습
    - 열실습

---
+++ 3_node_book_review/ch03/path.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const string = __filename;
+
+console.log('path.sep:',path.sep);
+console.log('path.delimiter:',path.delimiter);
+console.log('--------------');
+console.log('path.dirname():',path.dirname(string));
+console.log('path.extname():',path.extname(string));
+console.log('path.basename():',path.basename(string));
+console.log('path.basename():',path.basename(string, path.extname(string)));
+console.log('--------------');
+console.log('path.parse()',path.parse(string));
+console.log('path.format():',path.format({
+  dir: 'c:\\users\\smsr\\Documents\\node_book\\ch04',
+  name: 'path',
+  ext: '.js',
+}));
+console.log('path.normalize():',path.normalize('C://users\\\smsr\\\Documents\\\node_book\\\ch04\\\path.js'));
+console.log('--------------');
+console.log('path.isAbsolute():', path.isAbsolute('C:\\'));
+console.log('path.isAbsolute():',path.isAbsolute('./home'));
+console.log('--------------');
+console.log('path.reletive():',path.relative('C:\\users\\smsr\\Documents\\node_book\\ch04\\path.js', 'C:\\'));
+console.log('path.join():',path.join(__dirname,'..','..','/node_book','.','ch04'));
+console.log('path.resolve():',path.resolve(__dirname, '..', 'users', '.', '/smsr'));
