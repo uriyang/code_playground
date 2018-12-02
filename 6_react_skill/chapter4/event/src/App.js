dfe61b9ccb500708b262ed51c1c3becec99b60d5
uriyang <girlstoryk@gmail.com>
2018-12-02 21:43:20 +0900

    chapter4: 이벤트 핸들링
    - chapter4: 이벤트 핸들링 실습

---
+++ 6_react_skill/chapter4/event/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EventPratice from './EventPratice';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <EventPratice />
+      </div>
+    );
+  }
+}
+
+export default App;
