6583901232cc1556e3d828ea21b02e95baefb6a9
uriyang <girlstoryk@gmail.com>
2018-12-02 21:45:02 +0900

    chapter6: 컴포넌트 반복
    - chapter6: 컴포넌트 반복 실습

---
+++ 6_react_skill/chapter6/iterator/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import IterationSample from './IterationSample';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <IterationSample />
+      </div>
+    );
+  }
+}
+
+export default App;
