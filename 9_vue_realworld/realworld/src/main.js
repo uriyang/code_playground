e844f37124b63f89b5a91f94d292fc921bf9e693
seungmi kim <girlstoryk@gmail.com>
2019-01-07 21:18:20 +0900

    store 구조 생성
    - main.js store 붙히기
    - store 구조 생성

--- 9_vue_realworld/realworld/src/main.js
+++ 9_vue_realworld/realworld/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import store from './store';
+
+// TODO: 요기도 아직 붙혀야 할 기능들 있음
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
