d7db78ad92b0a03523bf96324ef01376c8f4cd70
seungmi kim <girlstoryk@gmail.com>
2019-01-07 21:34:11 +0900

    Header 링크 완료
    - Header 링크 완료

--- 9_vue_realworld/realworld/src/components/Header.vue
+++ 9_vue_realworld/realworld/src/components/Header.vue
@@ -5,12 +5,89 @@
       <router-link class="navbar-brand" :to="{ name: 'home'}">URIYANG</router-link>
       <!-- TODO: 요기 아랫 부분, VUEX 붙히고 나서 구현하기 -->
     </div>
+    <ul v-if="!isAuthenticated" class="nav navbar-nav pull-sx-right">
+      <li class="nav-item">
+        <!-- TODO: router-link 중복되는 것.. 중복되지 않게 수정 -->
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'home' }">
+          Home
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'login' }">
+          <i class="ion-compose"></i>Sign in
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'register' }">
+          <i class="ion-compose"></i>Sign up
+        </router-link>
+      </li>
+    </ul>
+    <ul v-else class="nav navbar-nav pull-xs-right">
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'home' }">
+          Home
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'article-edit' }">
+          <i class="ion-compose"></i>&nbsp;New Article
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ name: 'settings' }">
+          <i class="ion-gear-a"></i>&nbsp;Settings
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link
+          class="nav-link"
+          active-class="active"
+          exact
+          :to="{ 
+            name: 'profile',
+            params: { usernmae: currentUser.username }
+          }"
+        >
+          {{ currentUser.username }}
+        </router-link>
+      </li>
+    </ul>
   </nav>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
-  name: 'R-Header'
+  name: 'R-Header',
+  computed: {
+    ...mapGetters(["currentUser", "isAuthenticated"])
+  }
 }
 </script>
 
--- 9_vue_realworld/realworld/src/store/auth.module.js
+++ 9_vue_realworld/realworld/src/store/auth.module.js
@@ -1,5 +1,19 @@
-const state = {};
-const getters = {};
+import JwtService from "@/common/jwt.service"
+
+// TODO: 항목들 더 채워 넣어야 함
+const state = {
+  user: {},
+  isAuthenticated: !!JwtService.getToken()
+};
+
+const getters = {
+  currentUser(state) {
+    return state.user;
+  },
+  isAuthenticated(state) {
+    return state.isAuthenticated;
+  }
+};
 const actions = {};
 const mutations = {};
 
