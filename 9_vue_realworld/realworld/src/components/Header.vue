963a011d34bfc3f44af5b59ce096527595d25f1c
seungmi kim <girlstoryk@gmail.com>
2019-01-07 21:03:00 +0900

    Update Header.vue
    - header에 index 가는 router-lint 붙히기

--- 9_vue_realworld/realworld/src/components/Header.vue
+++ 9_vue_realworld/realworld/src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Header</div>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <!-- TODO: router name으로 링크 이동할때랑, path로 이동할 때 차이점 알아두기 -->
+      <router-link class="navbar-brand" :to="{ name: 'home'}">URIYANG</router-link>
+      <!-- TODO: 요기 아랫 부분, VUEX 붙히고 나서 구현하기 -->
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -7,3 +13,4 @@ export default {
   name: 'R-Header'
 }
 </script>
+
