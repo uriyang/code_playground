a6fc3b92d352d2bdb32773be9517f63eee0566d2
seungmi kim <girlstoryk@gmail.com>
2019-01-13 20:10:12 +0900

    chapter1: 시작하기 - 완료
    - 시작하기 완료

--- 11_haskel/baby.hs
+++ 11_haskel/baby.hs
@@ -4,3 +4,6 @@ doubleUS x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 
   then x 
   else x*2
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+length' xs = sum [1 | _ <- xs]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
