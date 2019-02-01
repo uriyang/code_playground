1e434e2eee89af31816b4736b6bad8fbd77942cd
kimseungmi <kimseungmi@huobi.com>
2019-02-01 17:35:41 +0900

    12: sample-page 문구수정
    - 문구 수정

--- 12_atomic-react/atomic-react/src/components/pages/SamplePage/index.js
+++ 12_atomic-react/atomic-react/src/components/pages/SamplePage/index.js
@@ -7,9 +7,9 @@ import {
 const SamplePage = () => {
   return (
     <PageTemplate header={<Header />} footer={<Footer />}>
-      <Heading>Heading</Heading>
+      <Heading>Irure mollit aute tempor incididunt eiusmod fugiat tempor aute ex sit aute est proident est.</Heading>
       <Paragraph>
-        Paragraph
+      Do cillum nulla consectetur excepteur aliquip adipisicing ipsum. Consectetur voluptate cillum cillum fugiat adipisicing eiusmod incididunt ut voluptate do aliquip ad irure occaecat cupidatat quis. Laborum laborum id quis officia anim quis in anim eu et aliquip sunt do excepteur. Consectetur ullamco sint do do nostrud tempor labore laboris sit fugiat veniam reprehenderit.
       </Paragraph>
     </PageTemplate>
   )
